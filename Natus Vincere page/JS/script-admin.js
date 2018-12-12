14c91f068601674c092dd93f8dc38e135a9b1d3e
1SerhiiDenysiuk <serhii.denysiuk.kn.2017@lpnu.ua>
2018-12-12 02:26:11 +0200

    added lab 9

--- Natus Vincere page/JS/script-admin.js
+++ Natus Vincere page/JS/script-admin.js
@@ -1,20 +1,75 @@
+var useLocalStorage = false;
+
+function News(image, title, text) {
+    this.image = image;
+    this.title = title;
+    this.text = text;
+}
+
 $(document).ready(function () {
-     images=JSON.parse(localStorage.getItem("images"));
-    titleNews=JSON.parse(localStorage.getItem("titleNews"));
-    textNews=JSON.parse(localStorage.getItem("textNews"));
-    if(titleNews==null){
-        images=[]
-        titleNews=[]
-        textNews=[]
+    if (useLocalStorage) {
+        images = JSON.parse(localStorage.getItem("images"));
+        titleNews = JSON.parse(localStorage.getItem("titleNews"));
+        textNews = JSON.parse(localStorage.getItem("textNews"));
+        if (titleNews == null) {
+            images = []
+            titleNews = []
+            textNews = []
+        }
     }
-     $('#add-news').click(function () {
-        if ($('#add-image').attr('src') != 'PNG/Image.png') {
-            if ($('#label-news').val().trim() != '' && $('#text-news').val().trim() != '') {
-                alert('Новина успішно додана');
+
+
+    $('#add-news').click(function () {
+        addDataToDB()
+    });
+
+
+
+    $('input[type=file]').change(function (event) {
+        var tmppath = URL.createObjectURL(event.target.files[0]);
+        $("#add-image").attr('src', tmppath);
+
+    });
+});
+
+function isOnline() {
+    return window.navigator.onLine;
+}
+
+function checkForms() {
+    if ($('#add-image').attr('src') != 'PNG/Image.png' && $('#label-news').val().trim() != '' && $('#text-news').val().trim() != '') {
+        return true;
+    } else {
+        if ($('#text-news').val().trim() == '') {
+            $('#text-news').css('border-color', '#FF2D2D');
+            $('#text-news').css('border-width', '4px');
+        } else {
+            $('#text-news').css('border-color', '#A9A9A9');
+            $('#text-news').css('border-width', '1px');
+        };
+        if ($('#label-news').val().trim() == '') {
+            $('#label-news').css('border-color', '#FF2D2D');
+            $('#label-news').css('border-width', '4px');
+        } else {
+            $('#label-news').css('border-color', '#A9A9A9');
+            $('#label-news').css('border-width', '1px');
+        }
+        if ($('#add-image').attr('src') == 'PNG/Image.png') {
+            alert('Додайте зображення')
+        }
+        return false;
+    }
+
+}
+
+function addDataToDB() {
+    if (checkForms()) {
+        if (!isOnline()) {
+            if (useLocalStorage) {
                 images.push("PNG/new_news.jpg");
                 titleNews.push($('#label-news').val());
                 textNews.push($('#text-news').val());
-                if(!isOnline()){
+                if (!isOnline()) {
                     localStorage.setItem("images", JSON.stringify(images));
                     localStorage.setItem("titleNews", JSON.stringify(titleNews));
                     localStorage.setItem("textNews", JSON.stringify(textNews));
@@ -23,26 +78,29 @@ $(document).ready(function () {
                 $('#label-news').css('border-width', '1px');
                 $('#text-news').css('border-color', '#A9A9A9');
                 $('#text-news').css('border-width', '1px');
-                
                 $('#text-news').val('');
                 $('#label-news').val('');
                 $('#add-image').attr("src", "PNG/Image.png");
+                $('input[type=file]').val("");
+            } else {
+                var request = indexedDB.open("news", 2);
+                request.onupgradeneeded = function (event) {
+                    var db = event.target.result;
+                    var objectStore = db.createObjectStore("news", {
+                        keyPath: "title"
+                    })
+                    objectStore.add(new News("PNG/new_news.jpg", $('#label-news').val().trim(), $('#text-news').val().trim()))
+                 
+                }
+                request.onsuccess = function (event) {
+                    var db = event.target.result;
+                    var objectStore = db.transaction(["news"], "readwrite").objectStore("news");
+                    request = objectStore.add(new News("PNG/new_news.jpg", $('#label-news').val().trim(), $('#text-news').val().trim()))
+                    request.onsuccess = function (event) {
+                        console.log("add new news ")
+                    }
+                }
+            }
+        }
+    }
 }
-}
-    });
-});
- $('input[type=file]').change(function (event) {
-        var tmppath = URL.createObjectURL(event.target.files[0]);
-         $("#add-image").attr('src', tmppath);
-     });
- function isOnline() {
-    return window.navigator.onLine;
-}
- window.addEventListener("online", function(){
-    sendData();
-    this.localStorage.removeItem("images");
-    this.localStorage.removeItem("titleNews");
-    this.localStorage.removeItem("textNews");
-});
- function sendData() {
- }
